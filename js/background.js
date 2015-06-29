3b57efa46bb555f36c7424272b81bc57e7c93d3a
李江 <lijiang@sina.com>
2015-06-29 12:59:11 +0800

    增加权限认证

--- js/background.js
+++ js/background.js
@@ -1,6 +1,14 @@
+var isAlpha = false,
+	$window; 
 
-function sendMessage(){
-	chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-	  	console.log(response.farewell);
-	});
+function getDomainFromUrl(url){
+	return url.match(/^(?:([A-Za-z]+):)?(\/{0,2})([0-9.\-A-Za-z]+)/)[3];
 }
+function checkForValidUrl(tabId, changeInfo, tab) {
+	$window = tab;
+	if(!/.ymatou.com|localhost|127.0.0.1/gi.test(getDomainFromUrl(tab.url))){
+		chrome.browserAction.disable(tabId);
+	}
+	isAlpha = /\.alpha\.ymatou.com|localhost|127.0.0.1/.test(tab.url);
+}
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
--- js/popup.js
+++ js/popup.js
@@ -1,15 +1,17 @@
+var background = chrome.extension.getBackgroundPage();
+var isAlpha = background.isAlpha,
+	url = background.$window.url;
 /**
  * 初始化 注册ymtapptools
  */
 function initTools(){
-
 	window.ymtappTools = {
 		//解析url成对象
-		parseUrl: function(str) {
+		parseUrl : function(str) {
 			var arr, 
 				part,
 				url = {};
-			if(!(str||"").replace(/^\s+|\s+$/,"")){
+			if(!(str||'').replace(/^\s+|\s+$/,'')){
 				return {};
 			}
 			arr = str.substring(1, str.length).split('&');
@@ -26,22 +28,23 @@ function initTools(){
  * @param  {strig} code  需要执行的代码
  */
 function tabsExecute(code,param){
-	param = param || "";
-	chrome.tabs.executeScript(null,{code:"("+String(code)+")("+param+")"});
+	param = param || '';
+	chrome.tabs.executeScript(null,{code:'('+String(code)+')('+param+')'});
 }
 
 //去tab中注册方法
 tabsExecute(initTools.toString());
 
-$("#login").click(function(){
+$('#login').click(function(){
 	var data = {};
-	data.UserName = $("[name=Username]").val();
-	data.Password = $("[name=Password]").val();
+	data.UserName = $('[name=Username]').val();
+	data.Password = $('[name=Password]').val();
+	var host = isAlpha?'api.alpha.ymatou.com':'api.app.ymatou.com' ;
 	$.ajax({
-		url:'http://api.alpha.ymatou.com/api/User/LoginAuth',
+		url:'http://'+host+'/api/User/LoginAuth',
 		method:'post',
 		data:data,
-		contentType:"application/x-www-form-urlencoded; charset=UTF-8",
+		contentType:'application/x-www-form-urlencoded; charset=UTF-8',
 		success:function(data){
 			if(data.Code == 200){
 				tabsExecute(function(data){
@@ -50,7 +53,7 @@ $("#login").click(function(){
 							var arr, 
 								part,
 								url = {};
-							if(!(str||"").replace(/^\s+|\s+$/,"")){
+							if(!(str||'').replace(/^\s+|\s+$/,'')){
 								return {};
 							}
 							arr = str.substring(1, str.length).split('&');
@@ -63,11 +66,11 @@ $("#login").click(function(){
 					var obj = parseUrl(search);
 					obj.UserId = data.UserId;
 					obj.AccessToken = data.AccessToken;
-					var queryStr="?";
+					var queryArr = [];
 					for(var i in obj){
-						queryStr += (i+"="+obj[i]+"&");
+						queryArr.push(i+'='+obj[i]);
 					}
-					window.location.search = queryStr.substring(1,queryStr.length-1);
+					window.location.search = '?'+queryArr.join('&');
 				},JSON.stringify(data.Data))
 				//window.close();
 			}else{
@@ -78,17 +81,15 @@ $("#login").click(function(){
 	})
 });
 
-$("#logout").click(function(){	
+$('#logout').click(function(){	
 	tabsExecute(function(){
-		//window.location.href = 'http://'+window.location.host+'/forYmatouApp/updateLogin?UserId=nil&AccessToken=nil';
-
 		document.createElement('img').src='http://'+window.location.host+'/forYmatouApp/updateLogin?UserId=nil&AccessToken=nil';
 		var search = window.location.search,
 			parseUrl = function(str) {
 				var arr, 
 					part,
 					url = {};
-				if(!(str||"").replace(/^\s+|\s+$/,"")){
+				if(!(str||'').replace(/^\s+|\s+$/,'')){
 					return {};
 				}
 				arr = str.substring(1, str.length).split('&');
@@ -101,22 +102,76 @@ $("#logout").click(function(){
 		var obj = parseUrl(search);
 		delete obj.UserId;
 		delete obj.AccessToken;
-		var queryStr="?";
+		var queryStr='?';
 		for(var i in obj){
-			queryStr += (i+"="+obj[i]+"&");
+			queryStr += (i+'='+obj[i]+'&');
 		}
 		window.location.search = queryStr.substring(1,queryStr.length-1);
 	})
 })
 
-$(".jump").click(function(){
+$('.jump').click(function(){
 	var me = $(this),
-		path = me.attr("data-path");
+		path = me.attr('data-path');
 	tabsExecute((function(path){
-		var host = window.location.host;
-		window.location.href= "http://"+host+"/forYmatouApp/"+path;
-	}),"\""+path+"\"");	
+		var host = window.location.host,
+			search = window.location.search,
+			parseUrl = function(str) {
+				var arr, 
+					part,
+					url = {};
+				if(!(str||'').replace(/^\s+|\s+$/,'')){
+					return {};
+				}
+				arr = str.substring(1, str.length).split('&');
+				for (var i in arr) {
+					part = arr[i].split('=');
+					url[part[0]] = part[1];
+				}
+				return url;
+			};
+		var obj = parseUrl(search);
+		var queryStr='?';
+		for(var i in obj){
+			queryStr += (i+'='+obj[i]+'&');
+		}
+		window.location.href= 'http://'+host+'/forYmatouApp/'+path+queryStr;
+	}),'\''+path+'\'');	
 })
 
-var data = chrome.extension.getBackgroundPage();
-data.sendMessage()
+$('#toProduct').click(function(){
+	var productId = $('[name=productId]').val();
+
+	tabsExecute((function(productId){
+		var host = window.location.host;
+		window.location.href= 'http://'+host+'/forYmatouApp/product/pid?pid='+productId;
+	}),'\''+productId+'\'');	
+});
+
+$('[name=productId]').change(function(){
+	var val = this.value,
+		attr = val.split('');
+	if(!/-/.test(val)){
+		attr.splice(8,0,'-')
+		attr.splice(13,0,'-')
+		attr.splice(18,0,'-')
+		attr.splice(23,0,'-');
+		this.value = attr.join('');
+	}
+})
+
+var LS_KEY = 'YMT_TOOLS_USERINFO';
+var userInfo = {
+	get:function(){
+		return JSON.parse(localStorage[LS_KEY] || JSON.stringify({
+			data:[{
+				userName:'lunchzhao',
+				password:'123456'
+			}]
+		}));
+	},
+	set:function(){
+		var storage = userInfo.get();
+	}
+}
+
